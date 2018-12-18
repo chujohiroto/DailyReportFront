3fbc14a2ad199877726abd64278a26a8e606fcb2
iRosSoftware <iros0522@gmail.com>
2018-12-18 18:11:17 +0900

    Updates

---
+++ precache-manifest.7c91f6b6e354a49b3578b824fb8fd067.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d466aa55582df9a33a4",
+    "url": "/DailyReportFront/static/css/main.4242d87e.chunk.css"
+  },
+  {
+    "revision": "9d466aa55582df9a33a4",
+    "url": "/DailyReportFront/static/js/main.9d466aa5.chunk.js"
+  },
+  {
+    "revision": "1a8176671425ce253298",
+    "url": "/DailyReportFront/static/css/1.49451883.chunk.css"
+  },
+  {
+    "revision": "1a8176671425ce253298",
+    "url": "/DailyReportFront/static/js/1.1a817667.chunk.js"
+  },
+  {
+    "revision": "087f6798f59feaeb347c",
+    "url": "/DailyReportFront/static/js/runtime~main.087f6798.js"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/DailyReportFront/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/DailyReportFront/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/DailyReportFront/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/DailyReportFront/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/DailyReportFront/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "0610d60a7614ee68db63250c15cdcb6e",
+    "url": "/DailyReportFront/index.html"
+  }
+];
