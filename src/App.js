b206f00f3b35307a13abccc39e555957ed7fc0f1
iRosSoftware <iros0522@gmail.com>
2018-12-18 19:31:45 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import request from 'superagent'
 class App extends Component {
 
   send(values){
-    alert(JSON.stringify(values));
-    request.post("http://home.irossoftware.com:9010/sendMessage")
+    request.post("https://home.irossoftware.com:9010/sendMessage")
             .send(values)
             .end(function(err, res) {
     if (err) {
       console.log(err);
+      alert('エラーが発生しました。');
     } else {
       console.log(res.status);
+      alert('送信しました。');
     }
   });
     // ここまで追記
